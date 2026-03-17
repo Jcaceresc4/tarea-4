8e2092805391d02a5f018ed17d622e2433d64467
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-16 23:00:12 -0600

    se realizo un arreglo de char

---
+++ Arreglo de Char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	char nombre[30];//string
+	cout<<"Ingrese Nombre Completo:";
+	//cin>>nombre;
+	cin.get(nombre,30);
+	cout<<nombre<<endl;
+	
+	
+	system("pause");
+}
