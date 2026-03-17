9d17befb1d2e14f1829ca906c2bdfd40d52ba089
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-16 23:39:20 -0600

    se agrego un ciclo for con matrices

---
+++ For con matrices.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	
+	//Arreglos bidimensionales, matriz, tabla
+	int fil=0,col=0;
+	cout<<"Ingrese las Filas: ";
+	cin>>fil;
+	cout<<"Ingrese las Columnas: ";
+	cin>>col;
+	int matriz[fil][col];
+	
+		cout<<"---------Ingresar---------"<<endl;
+	for (int i=0;i<fil;i++){
+		for(int ii=0;ii<col;ii++){
+			cout<<i<<","<<ii<<": ";
+			cin>>matriz[i][ii];
+		}
+		cout<<"____________________"<<endl;
+	}
+	
+	
+	cout<<"---------Mostrar---------"<<endl;
+	for (int i=0;i<fil;i++){
+		for(int ii=0;ii<col;ii++){
+			cout<<i<<","<<ii<<": "<<matriz[i][ii]<<endl;
+		}
+	}
+	system("pause");
+}
